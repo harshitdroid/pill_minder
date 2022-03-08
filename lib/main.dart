c0895f217290766a50060e04e80092c5e3ac614d
Harshit Rao <harshitdroid@gmail.com>
2022-03-07 21:59:40 -0800

    V0.02

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:pill_minder/Sign%20up%20Page.dart';
 import 'package:pill_minder/homePage.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 //import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
