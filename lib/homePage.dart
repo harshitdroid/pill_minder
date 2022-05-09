c9f96d99b11deef8d69ac45aa7b2771050e166cb
Harshit Rao <harshitdroid@gmail.com>
2022-05-09 00:48:13 -0700

    Added Dynamic update need to add   get medicine   listview   calender add   day of week later

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,11 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:pill_minder/addMed.dart';
 import 'package:pill_minder/main.dart';
 import 'package:pill_minder/profilePage.dart';
 import 'package:pill_minder/userInfo.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,10 +18,67 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+
 class _HomePageState extends State<HomePage> {
   final String userID = userInfo().getID();
-  @override
+  int _selectedIndex = 0;
+  var medList = [];
+  _HomePageState(){
+    refreshList();
+    FirebaseDatabase.instance.ref().child(userID).onChildChanged.listen((event) {
+      print("Data Changed");
+     refreshList();
+    });
+    FirebaseDatabase.instance.ref().child(userID).onChildRemoved.listen((event) {
+      print("Data removed");
+     refreshList();
+    });
+    FirebaseDatabase.instance.ref().child(userID).onChildAdded.listen((event) {
+      print("Data Added");
+     refreshList();
+    });
+    FirebaseDatabase.instance.ref().child(userID).onChildMoved.listen((event) {
+      print("Data Moved");
+     refreshList();
+    });
+  }
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: All',
+    ),
+    Text(
+      'Index 1: Bottle',
+    ),
+    Text(
+      'Index 2: School',
+    ),
+    Text(
+      'Index 3: Settings',
+    ),
+  ];
+  void refreshList() {
+    FirebaseDatabase.instance.ref().child(userID).get()
+        .then((datasnapshot) {
+      print("Successfully loaded data");
+      print(datasnapshot.key);
+      print(datasnapshot.value);
+      print("Iterating Value");
+      var tempList = [];
+      for ( var doc in datasnapshot.children) {
+        //print("${doc.key} => ${doc.value}");
+        tempList.add(doc.value);
+      }
+      print("Final List");
+      print(tempList);
+      medList = tempList;
+      setState(() {
 
+      });
+    }).catchError((error) {
+      print("Failed to load data");
+    });
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -72,20 +133,102 @@ class _HomePageState extends State<HomePage> {
                   focusedDay: DateTime.now(),
                 ),
               ),
-              // ListView(),
-              // ElevatedButton(onPressed: (){
-              // FirebaseFirestore.instance.collection(userID).get()
-              //     .then((querySnapShot){
-              //       print("successful");
-              //       print(querySnapShot);
-              //       querySnapShot.docs.forEach((element) {
-              //         print(element.data());
-              //       });
-              // }).catchError((error){
-              //   print("Failed");
-              // }
-              // );
-              // }, child: Text("get"))
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text("Name",
+
+                    style: TextStyle(
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 19,
+                    ),
+                  ),
+
+                  Text("Dosage",
+
+                    style: TextStyle(
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 19,
+                    ),
+                  ),
+                  Text("Type",
+
+                    style: TextStyle(
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 19,
+                    ),
+                  ),
+                  Text("Time",
+
+                    style: TextStyle(
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 19,
+                    ),
+                  ),
+
+                ],
+              ),
+              Flexible(
+                  child: ListView.builder(
+                      itemCount: medList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          height: 50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(10)
+                              ),
+                              color: Colors.indigo.withOpacity(0.1),
+                            ),
+                            margin: EdgeInsets.only(top: 2,bottom: 2, right: 10, left: 10),
+
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                width: 100,
+                                child: TextButton(
+                                  onPressed: (){},
+                                  child: Text('${medList[index]['medName']}',
+                                    textAlign: TextAlign.left,),
+                                ),
+                              ),
+                              Container(
+                                width: 40,
+                                child: TextButton(
+                                  onPressed: (){},
+                                  style: TextButton.styleFrom(backgroundColor: Colors.white.withOpacity(0.4)),
+                                  child: Text('${medList[index]['pills']}'),
+                                ),
+                              ),
+                              Container(
+                                width: 70,
+                                child: TextButton(
+                                  onPressed: (){},
+                                  child: Text('${medList[index]['Type']}'),
+                                ),
+                              ),
+                              Container(
+                                width: 70,
+                                child: TextButton(
+                                  onPressed: (){},
+                                  style: TextButton.styleFrom(backgroundColor: Colors.white.withOpacity(0.4)),
+                                  child: Text('${medList[index]['hour']}' + ':' + '${medList[index]['min']}'),
+                                ),
+                              ),
+
+
+
+                            ],
+                          ),
+                        );
+                      })
+              ),
             ],
           ),
 
@@ -100,22 +243,6 @@ class _HomePageState extends State<HomePage> {
     backgroundColor: Colors.blue.shade200,
     child: const Icon(Icons.add),
     ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.delete_outline_rounded),
-            label: 'Bottle',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.remove_circle),
-            label: 'Tablet',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_parking),
-            label: 'Pill',
-          ),
-        ],
-      ),
     );
 
   }
